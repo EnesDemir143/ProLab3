3909d5a50bcf982fe15c75c7a339632d6f0973a8
EnesDemir143 <enesd41@outlook.com>
2024-12-06 22:47:57 +0300

    Yazarlar listesi oluştu ve her yazar id ile atandı,Web uygulaması yapıldı

--- Main.py
+++ Main.py
@@ -1,6 +1,33 @@
+import time
+from dis import UNKNOWN
+
 import pandas as pd
 
-df = pd.read_excel("Data/PROLAB 3 - DATASET.xlsx")
-print(df["author_position"])
+class Author:
+    def __init__(self,name="Unknown"):
+        self.name = name
+        self.article= set();
+
+
+
+
+if __name__=='__main__':
+
+    start = time.time()
+    df = pd.read_excel("/Users/enesdemir/Desktop/ProLab3/Data/PROLAB 3 - DATASET.xlsx")
+    end = time.time()
+
+    ids=set()
+    authorsObjects=[]
+
+    for id,name in zip(df["orcid"],df["author_name"]):
+        if id not in ids:
+            ids.add(id)
+            authorsObjects.append(Author(name))
+
+
+
+    print(len(authorsObjects))
+    print(len(ids))
 
 
